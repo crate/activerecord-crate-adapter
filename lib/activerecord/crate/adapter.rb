f0f28bf022548595a563963afe8e0b7c4432312c
Christoph Klocker <christoph@vedanova.com>
2014-04-01 22:46:44 +0200

    initial commit

---
+++ lib/activerecord/crate/adapter.rb
@@ -0,0 +1,9 @@
+require "activerecord/crate/adapter/version"
+
+module Activerecord
+  module Crate
+    module Adapter
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/activerecord/crate/adapter/version.rb
@@ -0,0 +1,7 @@
+module Activerecord
+  module Crate
+    module Adapter
+      VERSION = "0.0.1"
+    end
+  end
+end

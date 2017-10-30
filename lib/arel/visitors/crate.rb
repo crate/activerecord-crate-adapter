4696f3b15297639c5a5b492e1a8a489abe104338
Christoph Klocker <christoph@vedanova.com>
2017-10-30 16:01:38 +0100

    fix visitor bug

--- lib/arel/visitors/crate.rb
+++ lib/arel/visitors/crate.rb
@@ -25,6 +25,18 @@ module Arel
 
       private
     end
+
+    class DepthFirst < Arel::Visitors::Visitor
+      alias :visit_Integer :terminal
+    end
+
+    class Dot < Arel::Visitors::Visitor
+      alias :visit_Integer :visit_String
+    end
+
+    class ToSql < Arel::Visitors::Visitor
+      alias :visit_Integer :literal
+    end
   end
 end
 

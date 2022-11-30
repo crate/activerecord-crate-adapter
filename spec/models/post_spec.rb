1659177fe011f7fb609ae891ff18650cf7e67d4b
Niklas Schmidtmer <niklas@crate.io>
2022-11-30 11:58:54 +0100

    Update rspec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -22,7 +22,6 @@
 require_relative '../spec_helper'
 
 describe Post do
-
   before(:all) do
     ActiveRecord::Migration.class_eval do
       create_table :posts do |t|
@@ -45,14 +44,13 @@ describe Post do
   context 'initialization' do
     it 'should initialize a post object with all columns' do
       post = Post.new(params)
-      post.should be_a(Post)
-      post.title.should eq "Crate rocks"
-      post.views.should eq 10000
+      expect(post).to be_a(Post)
+      expect(post.title).to eq("Crate rocks")
+      expect(post.views).to eq(10000)
     end
   end
 
   context 'persistance' do
-
     before do
       @post = Post.create!(params)
     end
@@ -62,22 +60,20 @@ describe Post do
     end
 
     it 'should persist the record to the database' do
-      @post.persisted?.should eq true
+      expect(@post.persisted?).to be(true)
       refresh_posts
-      Post.count.should eq 1
+      expect(Post.count).to eq(1)
     end
-
   end
 
   context 'deletion' do
-
     before do
       @post = Post.create!(params)
     end
 
     it 'should destroy the record to the database' do
       @post.destroy
-      Post.where(id: @post.id).should be_empty
+      expect(Post.where(id: @post.id)).to be_empty
     end
   end
 
@@ -93,28 +89,27 @@ describe Post do
     context 'find' do
       it 'should find the crated record' do
         post = Post.where(id: @post.id).first
-        post.id.should eq(@post.id)
+        expect(post.id).to eq(@post.id)
       end
 
       it 'should find the crated record by title' do
         refresh_posts
-        Post.where(title: @post.title).count.should eq 1
+        expect(Post.where(title: @post.title).count).to be(1)
         post = Post.where(title: @post.title).first
-        post.id.should eq(@post.id)
+        expect(post.id).to eq(@post.id)
       end
     end
 
     context 'update' do
       it 'should update the record' do
         @post.update_attributes(title: 'Crate Dope')
-        @post.reload.title.should eq('Crate Dope')
+        expect(@post.reload.title).to eq('Crate Dope')
       end
 
     end
   end
 
   describe 'sql input sanitization' do
-
     before do
       @post = Post.create!(params)
     end
@@ -125,16 +120,13 @@ describe Post do
 
     it 'should not return all records but sanitize string' do
       sql = Post.where(id: "#{@post.id} or 1=1").to_sql
-      sql.should match(/'#{@post.id} or 1=1'/)
+      expect(sql).to match(/'#{@post.id} or 1=1'/)
     end
 
     it 'should not drop the table but sanitize string' do
       Post.where(id: "#{@post.title}; DROP TABLE POST")
       refresh_posts
-      Post.last.id.should eq @post.id
+      expect(Post.last.id).to eq(@post.id)
     end
-
   end
-
 end
-
